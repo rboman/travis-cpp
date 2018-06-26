277a6b74c9f60a553915662675e7b29e99e750d9
Romain Boman <romain.boman@gmail.com>
2018-06-26 16:47:54 +0200

    use c++11

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
     std::cout << "Hello!\n";
+
+    std::vector<double> vec = {1,2,3};
+    for(auto v : vec)
+        std::cout << v << std::endl;
 
     return 0;
 }
